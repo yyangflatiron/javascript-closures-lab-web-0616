05cd04e792f5f4c56bde827717f829b552d10762
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-25 20:25:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much."
+function bumpCounter(){
+  var counter = 0
+  function addBump(){counter++}
+  function getBumps(){return counter}
+  return {addBump, getBumps}
+}
+
+function createAnimal(animalType){
+  return (deadlyDevice)=>{
+    return {animalType,deadlyDevice} //
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
